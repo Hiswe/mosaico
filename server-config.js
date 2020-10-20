59f3a31bbfaa65df3230d7ada23d27c97414386e
Carri <studiocoma.be@gmail.com>
2020-10-20 13:58:09 -0500

    port

--- server-config.js
+++ server-config.js
@@ -1,12 +1,12 @@
 // Put here your email sending configurations!
 module.exports = {
 	emailTransport: {
-		host: 'mail.monarchalpha.com',
-		port: 465,
-		auth: { user: 'monarch@monarchalpha.com', pass: 'fuv2iw*K4M~W' }
+		host: "mail.monarchalpha.com",
+		port: 26,
+		auth: {user: "monarch@monarchalpha.com", pass: "fuv2iw*K4M~W"}
 	},
 	emailOptions: {
-		from: 'Monarch <monarch@monarchalpha.com>', // sender address
-		// bcc: 'mosaico@mosaico.io',
+		from: "Monarch <monarch@monarchalpha.com>", // sender address
+		bcc: "monarch@monarchalpha.com>o",
 	}
 };
